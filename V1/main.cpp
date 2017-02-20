8a0fc7cd2b384a9c7c3616bb89313210842fffb3
vrublevskiyvitaliy <vrublevskiy@lun.ua>
2017-02-20 09:53:24 +0200

    Initial Commit

---
+++ V1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  V1
+//
+//  Created by Vitaliy Vrublevskiy on 2/20/17.
+//  Copyright © 2017 Vitaliy Vrublevskiy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
