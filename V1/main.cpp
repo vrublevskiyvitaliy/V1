581d1eee56bf830b8848c0f96c45b547a32265d4
vrublevskiyvitaliy <vrublevskiy@lun.ua>
2017-03-24 18:59:48 +0200

    add shader and opengl code

--- V1/main.cpp
+++ V1/main.cpp
@@ -20,6 +20,7 @@ using namespace glm;
 #include "shader.hpp"
 #include "Points.hpp"
 #include "OpenGLHelper.hpp"
+#include "KDTree.hpp"
 
 const int numberOfPoints = 100;
 const bool useMouse = false;
@@ -88,6 +89,7 @@ int main( void )
     GLuint vsiPosition = glGetAttribLocation(programID, "vsiPosition");
     GLuint pointPositions = glGetUniformLocation(programID, "pointPositions");
     GLuint pointColors = glGetUniformLocation(programID, "pointColors");
+    GLuint kdTreeSize = glGetUniformLocation(programID, "UN_SAMP_KDTREE_SIZE");
     
     // Model matrix : an identity matrix (model will be at the origin)
     glm::mat4 Model      = glm::mat4(1.0f);
@@ -113,14 +115,65 @@ int main( void )
     glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
     
+    GLuint vertexTexture;
+    GLuint locTex = glGetUniformLocation(programID, "vertexTexture");
+    
+    KDTree tree;
+    
+    double lastTime = glfwGetTime(); int nbFrames = 0;
+    double lastTimeTree = glfwGetTime();
+    double lastTimeLoop = glfwGetTime();
     int counter = 0;
     do{
+        double currentTime = glfwGetTime();
+        double currentTimeLoop = glfwGetTime();
+        printf("%f time for loop\n", currentTimeLoop - lastTimeLoop);
+        lastTimeLoop = glfwGetTime();
+        nbFrames++;
+        if ( currentTime - lastTime >= 1.0 ){ // If last prinf() was more than 1 sec ago
+            // printf and reset timer
+            printf("%f ms/frame or %f fps\n", 1000.0/double(nbFrames), double(nbFrames));
+            nbFrames = 0;
+            lastTime += 1.0;
+        }
+        
+        
+        lastTimeTree = glfwGetTime();
+            tree.setData();
+            int n = tree.getNumNodes();
+            std::vector<float> v_data = tree.getData();
+        
+            float * data = new float[n * 4];
+        
+            for(int i=0; i < n * 4; i++)
+            {
+                data[i]=v_data[i];
+            }
+        double currentTimeTree = glfwGetTime();
+        
+        printf("%f time for tree \n", currentTimeTree - lastTimeTree);
+
+            glm::vec2 kdTreeSizeValue = glm::vec2(n, 1);
+        
+            glGenTextures(1, &vertexTexture);
+            glActiveTexture(GL_TEXTURE0);
+            glBindTexture(GL_TEXTURE_2D, vertexTexture);
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, n, 1, 0, GL_RGBA, GL_FLOAT, data);
+        
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        
         
         // Clear the screen
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
         // Use our shader
         glUseProgram(programID);
+        
+        glUniform1i(locTex, 0);
+        glUniform2f(kdTreeSize, kdTreeSizeValue.x, kdTreeSizeValue.y);
         
         // Send our transformation to the currently bound shader,
         // in the "MVP" uniform
@@ -158,6 +211,8 @@ int main( void )
         // Swap buffers
         glfwSwapBuffers(window);
         glfwPollEvents();
+        
+        
         
     } // Check if the ESC key was pressed or the window was closed
     while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
