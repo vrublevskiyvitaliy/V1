a84ffc2ec0189861709c69805024f2c39f5bd8a1
vrublevskiyvitaliy <vrublevskiy@lun.ua>
2017-03-25 20:13:53 +0200

    add QuickSelect

--- V1/test_kd_tree.cpp
+++ V1/test_kd_tree.cpp
@@ -15,11 +15,17 @@
 using namespace std;
 
 #include "QuickSort.hpp"
+#include "QuickSelect.hpp"
+
 
 int __main()
 {
-    QuickSort::testQuickSort(5, 0);
- /*
+    //QuickSort::testQuickSort(5, 0);
+    
+    //QuickSelect q;
+    //q.testSort(6, 0);
+ 
+    /*
     KDTree tree(4);
     tree.setData();
     
